6abb2883a4ba29278514d43cfc092b438298284d
Nagesh <nageshkolkar46@gmail.com>
2023-12-04 00:40:30 +0530

    First Commit

--- auth.py
+++ auth.py
@@ -249,10 +249,13 @@ def addvalues():
     water = request.args.get('water')
     coffee_powder = request.args.get('coffee')
     amount = request.args.get('amount')
-    coffee_instance = Coffee(coffee_name=coffeename, milk_required_ml=milk, water_required_ml=water, coffee_require_ml=coffee_powder,
+    coffee_instance = Coffee(coffee_name=coffeename, milk_required_ml=milk, water_required_ml=water,
+                             coffee_require_ml=coffee_powder,
                              amount=amount)
     db.session.add(coffee_instance)
     db.session.commit()
+    return "added"
+
 
 @app.route("/add_resource")
 def addresource():
@@ -260,9 +263,12 @@ def addresource():
     water = request.args.get('water')
     coffee_powder = request.args.get('coffee')
     amount = request.args.get('amount')
-    coffee_instance = Resources( milk_stock=milk, water_stock=water, coffee_powder_stock=coffee_powder,
-                             wallet=amount)
+    coffee_instance = Resources(milk_stock=milk, water_stock=water, coffee_powder_stock=coffee_powder,
+                                wallet=amount)
     db.session.add(coffee_instance)
     db.session.commit()
+    return "added"
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=5010)

0734cb2d4a4d357402f274a8ed5a701894c9954e
Nagesh <nageshkolkar46@gmail.com>
2023-12-04 10:59:11 +0530

    Now

--- auth.py
+++ auth.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///coffeemachine.db'
 db = SQLAlchemy()
